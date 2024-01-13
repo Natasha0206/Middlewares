d84870f46b4cd3f28f23c72acd9e526893d764cf
Natasha Kumari <140797294+Nxtxshx@users.noreply.github.com>
2024-01-13 09:38:39 +0530

    Add files via upload

---
+++ globalCatches.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.post("/health-checkup", function (req,res){
+    const kidneys = req.body.kidneys;
+    const kidneyLength = kidneys.length;
+    res.send("you have"+ kidneyLength + "kidneys");
+});
+
+//global catches
+app.use(function(err,req,res,next){
+    res.json({
+        msg:"Sorry something is wrong with our server"
+    })
+})
+app.listen(3000);

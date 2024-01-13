b93fafc0c2a1e7bd6ebfa1236ffc0dbee21f06d0
Natasha Kumari <140797294+Nxtxshx@users.noreply.github.com>
2024-01-13 10:53:41 +0530

    Add files via upload

---
+++ simplifyingZod.js
@@ -0,0 +1,8 @@
+const zod = require("zod");
+
+function validateInput(arr){
+    const schema = zod.array(zod.number());
+    const response = schema.safeParse(arr);
+    console.log(response);
+}
+validateInput([1,2,3]);
